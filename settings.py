09cd54bd9c0b09e560e86d5cf45194b83268c48f
Bob Haugen <bob.haugen@gmail.com>
2010-08-05 09:48:41 -0500

    renamed database

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'fad.sqlite'             # Or path to database file if using sqlite3.
+DATABASE_NAME = 'foodnetwork.sqlite'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
